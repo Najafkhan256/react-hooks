1f3d8ce9643bbf02836555ed1319db5470e1242c
Najafkhan256 <najafkhan6625@gmail.com>
2023-09-30 14:05:41 +0500

    useReduser Hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,72 +1,39 @@
-/**
- * The above code is a React application that uses the useContext hook to create a counter with
- * increment and decrement functionality.
- * @returns The App component is returning the CounterProvider component, which wraps the
- * IncrementCounter and DecrementCounter components.
- */
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useReducer, useState } from "react";
 import "./App.css";
 
-const CounterContext = createContext();
+const initialState = { backgroundColor: "#cecece" };
 
-const CounterProvider = ({ children }) => {
-  const [count, setCount] = useState(0);
-
-  const increment = () => setCount((prevCount) => prevCount + 1);
-  const decrement = () => setCount((prevCount) => prevCount - 1);
-
-  const contextValue = {
-    count,
-    increment,
-    decrement,
-  };
-
-  return (
-    <CounterContext.Provider value={contextValue}>
-      {children}
-    </CounterContext.Provider>
-  );
-};
-
-const IncrementCounter = () => {
-  const { increment } = useContext(CounterContext);
-
-  return (
-    <div>
-      <button onClick={increment}>Increment</button>
-    </div>
-  );
-};
-
-const DecrementCounter = () => {
-  const { count, decrement } = useContext(CounterContext);
-
-  return (
-    <div>
-      {count === 0 ? (
-        <button disabled style={{ opacity: "50px" }}>
-          Decrement
-        </button>
-      ) : (
-        <button onClick={decrement}>Decrement</button>
-      )}
-    </div>
-  );
-};
-
-const ShowResult = () => {
-  const { count } = useContext(CounterContext);
-  return <h2>{count}</h2>;
+const reducer = (state, action) => {
+  switch (action) {
+    case "black": {
+      return { backgroundColor: "#000" };
+    }
+    case "red": {
+      return { backgroundColor: "#f00" };
+    }
+    default: {
+      return { backgroundColor: "initial" };
+    }
+  }
 };
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
   return (
-    <CounterProvider>
-      <h2>useContext Hook</h2>
-      <ShowResult />
-      <IncrementCounter />
-      <DecrementCounter />
-    </CounterProvider>
+    <>
+      <h2
+        style={{ backgroundColor: state.backgroundColor, padding: "40px 20px" }}
+      >
+        useContext Hook
+      </h2>
+
+      <button onClick={() => dispatch("red")}>
+        Change Background into red
+      </button>
+      <button onClick={() => dispatch("black")}>
+        Change Background into Black
+      </button>
+    </>
   );
 }
 
